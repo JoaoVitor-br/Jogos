8752d7e6339ffaa857a0039e4c2af13e8ed219a1
JoaoVitor-br <amorim.jao3@gmail.com>
2026-02-27 17:00:22 -0300

    .

--- foguete/script.js
+++ foguete/script.js
@@ -1,18 +1,35 @@
-
-let score = 0;
+let tempo = 0;
 let gameInterval;
+// function abertura() {
+//     document.getElementById("menu").style.display = "none";
+//     document.getElementById("abertura").style.display = "block";
+//     document.querySelector("body").style.display = "none"
+//     tempo = 0;
+//     gameInterval = setInterval(() => {
+
+
+//         tempo++;
+//         document.getElementById("tempo").innerText = tempo;
+//     }, 1000);
+
+
+// }
+console.log(tempo)
 
 function startGame() {
     document.getElementById("menu").style.display = "none";
+
     document.getElementById("game").style.display = "block";
-    
-    score = 0;
-    document.getElementById("score").innerText = score;
+    document.querySelector("header").style.width = "0vw"
+    document.querySelector("header").style.height = "0vh"
+
+
+    document.getElementById("tempo").innerText = tempo;
 
     // Simulação de pontuação automática
     gameInterval = setInterval(() => {
-        score++;
-        document.getElementById("score").innerText = score;
+        tempo++;
+        document.getElementById("tempo").innerText = tempo;
     }, 1000);
 }
 
